78f74cf1db1661f2a8fd10f2d95fbc883e98da35
Michael Hamilton <michaelhamilton626@gmail.com>
2016-11-16 14:49:51 -0500

    Added Quartz for frontend styles

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./controllers/index');
 
 var app = express();
 
@@ -27,9 +26,9 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
