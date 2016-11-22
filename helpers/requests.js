4a36d478f452d1911eb7a7a97a4cb2f7e61e43d1
Michael Hamilton <michaelhamilton626@gmail.com>
2016-11-22 15:36:38 -0500

    Adds form validation and fixes issue that are caused by non validated forms

--- helpers/requests.js
+++ helpers/requests.js
@@ -11,6 +11,25 @@ module.exports = {
 
     //Validates inputs from form submission.  Checks all checkbox inputs and converts their values to true or false
     validateInputs: function(req, res, next) {
+        req.body.event.is_event = req.body.event.is_event=="on" ? true : false;
+
+        req.body.mediums = req.body.mediums ? req.body.mediums : {};
+        req.body.mediums.app = req.body.mediums.app=="on" ? true : false;
+        req.body.mediums.announcements = req.body.mediums.announcements=="on" ? true : false;
+        req.body.mediums.before_mass_slides = req.body.mediums.before_mass_slides=="on" ? true : false;
+        req.body.mediums.endnotes = req.body.mediums.endnotes=="on" ? true : false;
+        req.body.mediums.online_campus = req.body.mediums.online_campus=="on" ? true : false;
+        req.body.mediums.website = req.body.mediums.website=="on" ? true : false;
+        req.body.mediums.worship_fully = req.body.mediums.worship_fully=="on" ? true : false;
+        req.body.mediums.facebook = req.body.mediums.facebook=="on" ? true : false;
+        req.body.mediums.instagram = req.body.mediums.instagram=="on" ? true : false;
+        req.body.mediums.twitter = req.body.mediums.twitter=="on" ? true : false;
+
+        req.body.dates = req.body.dates ? req.body.dates : {};
+        for(var date in req.body.dates) {
+            req.body.dates[date] = req.body.dates[date]=="on" ? true : false;
+        }
+
         next();
     },
 
